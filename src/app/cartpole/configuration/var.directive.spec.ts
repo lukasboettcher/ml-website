c9e40c1255fa240b041c074d2156ad52a47a94d6
Lukas Boettcher <stu210239@mail.uni-kiel.de>
2021-04-17 00:31:08 +0200

    remove some legacy tests

--- src/app/cartpole/configuration/var.directive.spec.ts
+++ src/app/cartpole/configuration/var.directive.spec.ts
@@ -2,7 +2,7 @@ import { VarDirective } from './var.directive';
 
 describe('VarDirective', () => {
   it('should create an instance', () => {
-    const directive = new VarDirective();
+    const directive = new VarDirective(null, null);
     expect(directive).toBeTruthy();
   });
 });
