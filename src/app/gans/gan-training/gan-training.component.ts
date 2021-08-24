1c8e760a9f72ac53a20ac57c1ce74163a76065c5
Lukas Boettcher <stu210239@mail.uni-kiel.de>
2021-08-24 21:08:03 +0200

    created builderplate for new gan components namely: taining and transfer

---
+++ src/app/gans/gan-training/gan-training.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gan-training',
+  templateUrl: './gan-training.component.html',
+  styleUrls: ['./gan-training.component.css']
+})
+export class GanTrainingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/gans/gan-transfer/gan-transfer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gan-transfer',
+  templateUrl: './gan-transfer.component.html',
+  styleUrls: ['./gan-transfer.component.css']
+})
+export class GanTransferComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
