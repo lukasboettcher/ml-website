914d23344c56b18c38f54675738e5a38c59feecc
Lukas Boettcher <lukas@btchr.de>
2021-08-26 22:48:31 +0200

    import needed code in the gan style transfer component tfjs and child component most notably

--- src/app/gans/gan-transfer/gan-transfer.component.ts
+++ src/app/gans/gan-transfer/gan-transfer.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import descriptions from './image-descriptions';
+import * as tf from '@tensorflow/tfjs';
+import { GanTransferInputComponent } from './gan-transfer-input/gan-transfer-input.component';
 
 @Component({
   selector: 'app-gan-transfer',
