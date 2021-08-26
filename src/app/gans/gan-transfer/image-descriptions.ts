5fceb9b1c2027b5af9241470deafdf0bb4be8990
Lukas Boettcher <lukas@btchr.de>
2021-08-26 21:57:51 +0200

    created a struct for the image descriptions these are offered to the user as inputs / styles for the gan transfer component most of the images are taken from https://github.com/magenta/magenta/tree/main/magenta/models/arbitrary_image_stylization the images are deployed as assets together with the website

---
+++ src/app/gans/gan-transfer/image-descriptions.ts
@@ -0,0 +1,25 @@
+export default {
+    inputs: [
+        { path: 'stata', name: 'Modernes Gebäuse' },
+        { path: 'diana', name: 'Prinzessin Diana' },
+        { path: 'golden_gate', name: 'Golden Gate Bridge' },
+        { path: 'beach', name: 'Strand' },
+        { path: 'chicago', name: 'Chicago' },
+        { path: 'statue_of_liberty', name: 'Freiheitsstatue' },
+        { path: 'nyc_skyline', name: 'New York City' },
+    ],
+    styles: [
+        { path: 'random', name: 'Random Bild von wikiart.org' },
+        { path: 'udnie', name: 'Udnie' },
+        { path: 'stripes', name: 'Streifen' },
+        { path: 'bricks', name: 'Steine' },
+        { path: 'clouds', name: 'Wolken' },
+        { path: 'towers', name: 'Türme' },
+        { path: 'sketch', name: 'Zeichnung' },
+        { path: 'seaport', name: 'Hafen' },
+        { path: 'red_circles', name: 'Rote Kreise' },
+        { path: 'zigzag', name: 'Zickzack' },
+        { path: 'starry_night', name: 'Starry Night' },
+        { path: 'wave', name: 'Die Welle' },
+    ]
+};
