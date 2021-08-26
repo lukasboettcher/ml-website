9ee3e4df08228cffda4335a19853a30f48496cd6
Lukas Boettcher <lukas@btchr.de>
2021-08-26 21:53:18 +0200

    added two simple methods in gan training to sample from either mnist directly or the pretrained model the resulting digit images are drawn on a canvas element

--- src/app/gans/gan-training/gan-training.component.ts
+++ src/app/gans/gan-training/gan-training.component.ts
@@ -23,10 +23,33 @@ export class GanTrainingComponent implements OnInit {
     Promise.all([this.loadPretrained(), loadMnistData()]).then(() => {
       this.modelsLoaded = true;
     });
+  }
+
   async loadPretrained(): Promise<void> {
     this.pretrainedModel = await tf.loadLayersModel(this.MODEL_PATH);
   }
 
+
+  async sampleFromPretrained(genCanvas: HTMLCanvasElement): Promise<void> {
+    if (this.pretrainedModel) {
+      const generatedImages = tf.tidy(() => {
+        const classLabels = tf.tensor2d([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [10, 1]);
+        const latentVector = tf.randomUniform([10, this.LATENT_DIM]);
+
+        const modelOutput = (this.pretrainedModel.predict([latentVector, classLabels]) as tf.Tensor).add(1).div(2);
+        return tf.concat(tf.unstack(modelOutput), 1) as tf.Tensor2D;
+      });
+
+      await tf.browser.toPixels(generatedImages, genCanvas);
+
+      tf.dispose(generatedImages);
+    }
+  }
+
+  async sampleFromMnist(realCanvas: HTMLCanvasElement): Promise<void> {
+    const combinedReals = sampleFromMnistData(10);
+    await tf.browser.toPixels(combinedReals, realCanvas);
+    tf.dispose(combinedReals);
   }
 
 }
