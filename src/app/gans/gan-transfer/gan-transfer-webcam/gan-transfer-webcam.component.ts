d741501bafd912378a504404a9faa6465e1dcd67
Lukas Boettcher <stu210239@mail.uni-kiel.de>
2021-08-26 05:27:56 +0200

    created the webcam component this component should allow the user to take pictures and emit them as events to the parent

---
+++ src/app/gans/gan-transfer/gan-transfer-webcam/gan-transfer-webcam.component.ts
@@ -0,0 +1,12 @@
+import { AfterViewInit, Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+@Component({
+  selector: 'app-gan-transfer-webcam',
+  templateUrl: './gan-transfer-webcam.component.html',
+  styleUrls: ['./gan-transfer-webcam.component.css']
+})
+export class GanTransferWebcamComponent implements OnInit, AfterViewInit {
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
