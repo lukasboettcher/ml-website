a77ddc6a23ea77e8f72d424a7b583282fa9a8036
Lukas Boettcher <stu210239@mail.uni-kiel.de>
2021-08-26 22:40:40 +0200

    added method that handles change of the select element in the gan input component differentiate between file, webcam, random remote, and direct selection this was inspired from https://github.com/reiinakano/arbitrary-image-stylization-tfjs

--- src/app/gans/gan-transfer/gan-transfer-input/gan-transfer-input.component.ts
+++ src/app/gans/gan-transfer/gan-transfer-input/gan-transfer-input.component.ts
@@ -22,6 +22,31 @@ export class GanTransferInputComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  async onSelectChange(element: HTMLImageElement, select: HTMLSelectElement, uploadElem: HTMLInputElement): Promise<void> {
+    const value = select.value;
+    if (value === 'file') {
+      console.log('file selected');
+      uploadElem.onchange = (evt) => {
+        const f = (evt.target as HTMLInputElement).files[0];
+        const fileReader = new FileReader();
+        fileReader.onload = ((e) => {
+          element.src = (e.target.result as string);
+        });
+        fileReader.readAsDataURL(f);
+        uploadElem.value = '';
+      };
+      uploadElem.click();
+    } else if (value === 'pic') {
+      // handle this explicitly on the click event
+    } else if (value === 'random') {
+      const randomNumber = Math.floor(Math.random() * links.length);
+      element.src = links[randomNumber];
+    } else if (value !== '') {
+      element.src = 'assets/gan-images/' + value + '.jpg';
+    }
+    // reset the select at the end
+    select.value = '';
+  }
   onNewImage(imageData: string): void {
     this.image.nativeElement.src = imageData;
   }
