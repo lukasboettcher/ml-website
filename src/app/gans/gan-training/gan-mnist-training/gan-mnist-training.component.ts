2122ba80e36d9c3829b5edfd4b22134de75592ca
Lukas Boettcher <stu210239@mail.uni-kiel.de>
2021-08-30 06:58:36 +0200

    created seperate gan mnist trianign component, so that the code is more readable

---
+++ src/app/gans/gan-training/gan-mnist-training/gan-mnist-training.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+@Component({
+  selector: 'app-gan-mnist-training',
+  templateUrl: './gan-mnist-training.component.html',
+  styleUrls: ['./gan-mnist-training.component.css']
+})
+export class GanMnistTrainingComponent implements OnInit {
+
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+}
