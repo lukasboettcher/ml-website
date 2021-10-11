e527b9f9e9b13082bdad7d58871c99a901f42899
Lukas Boettcher <lukas@btchr.de>
2021-10-11 15:19:34 +0200

    imported the rest of the js code WIP converting to ts

---
+++ src/app/tictactoe/agent.ts
@@ -0,0 +1,285 @@
+import { Board } from './boardModel';
+/**
+ * Class to create an intelligent agent object which learns via reinforcement learning for the game tic-tac-toe.
+*/
+
+class Agent {
+
+    private opponent: number;
+    private playerSymbol: number;
+    private playsInCurrentGame: number[][];
+    private states: number[];
+    private deletedStatesInCurrentGame: number[];
+    private hasLearned: boolean;
+    private emptyKeys: number[];
+
+
+    /**
+     * Create a new Agent object for a game of tic-tac-toe.
+     */
+    constructor(playerSymbol, opponent) {
+        // the integer representation of its opponent
+        this.opponent = opponent;
+        // its own integer representation
+        this.playerSymbol = playerSymbol;
+        // array to track which moves where made within a game
+        this.playsInCurrentGame = [];
+        // the policy of the agent
+        this.states = [];
+        // tracks which states where deleted through a game
+        this.deletedStatesInCurrentGame = [];
+        // tracks if the learn process for a single game was finished
+        this.hasLearned = false;
+        // create a initial empty board for creation of the policy
+        const board = new Board(this.playerSymbol, this.opponent);
+        // create the policy
+        this.getAllPossibleStates(board, opponent);
+        // tracks which Keys where emptied during the learning process
+        this.emptyKeys = [];
+    }
+    /**
+     * Method to clear the possible states from empty keys and values from other keys which where learned not to make.
+     * Also clears the deletedStatesInCurrentGame and playsInCurrentGame 
+     * attributes and resets the hasLearned attribute to false for a new game.
+     */
+    reset(): void {
+
+        while (this.emptyKeys.length !== 0) {
+            delete this.states[this.emptyKeys[0]];
+            this.deleteValuesLeadingToKey(this.emptyKeys[0]);
+            this.emptyKeys = this.emptyKeys.slice(1, this.emptyKeys.length);
+        }
+        this.deletedStatesInCurrentGame = [];
+        this.playsInCurrentGame = [];
+        this.hasLearned = false;
+
+    }
+
+    /**
+     * Method to choose a move given a board situation. 
+     * Is chosen at random, when there are multiple move to choose from. Else the only possible move.
+     * @returns the move, the agent makes.
+     */
+    chooseAction(board): number {
+        const copy = board.deepCopy();
+        copy.getMinimalSymmetry();
+        const key = JSON.stringify(copy.board);
+        // There is just the last position remaining. So no decisions possible.
+        if (this.states[key] === undefined) {
+            if (copy.getAvailablePositions().length > 1) {
+                // Something went horribly wrong.
+                // resetAll();
+                location.reload();
+
+            }
+            copy.invertBoardToActualOne();
+            return copy.getAvailablePositions()[0];
+        } else {
+            const posMoves = this.states[key];
+            let index = 0;
+            if (posMoves.length > 1) {
+                index = Math.floor(Math.random() * posMoves.length);
+            }
+            this.playsInCurrentGame.push([key, posMoves[index]]);
+            const boardCopy = posMoves[index].deepCopy();
+            boardCopy.inverseIndex = copy.inverseIndex;
+            boardCopy.invertBoardToActualOne();
+            return board.getPosition(boardCopy);
+        }
+    }
+
+    /**
+     * Method which learns the agent its strategy over a period of games.
+     */
+    learn(result): void {
+        if (result === this.opponent) {
+            while (!this.hasLearned) {
+                const key = this.playsInCurrentGame[this.playsInCurrentGame.length - 1][0];
+                const moveLeadingToLoss = this.playsInCurrentGame[this.playsInCurrentGame.length - 1][1].deepCopy();
+                this.deletedStatesInCurrentGame.push(moveLeadingToLoss.deepCopy());
+                const wasLastOne = this.deleteValueInKey(key, moveLeadingToLoss);
+                if (wasLastOne) {
+                    this.hasLearned = true;
+                } else {
+                    this.playsInCurrentGame = this.playsInCurrentGame.slice(0, this.playsInCurrentGame.length - 1);
+                }
+            }
+
+        }
+    }
+
+    /**
+     * Method to delete the value of the given board, found in key, in the strategy of the agent.
+     * @returns true, if the key still have values left, else false.
+     */
+    deleteValueInKey(key, board): boolean {
+        const deletingVal = board.deepCopy();
+        deletingVal.getMinimalSymmetry();
+        for (let i = 0; i < this.states[key].length; i++) {
+            const state = this.states[key][i].deepCopy();
+            state.getMinimalSymmetry();
+            if (JSON.stringify(state.board) == JSON.stringify(deletingVal.board)) {
+                delete this.states[key][i];
+            }
+        }
+        this.states[key] = this.states[key].filter(el => el);
+        if (this.states[key].length == 0) {
+            this.emptyKeys.push(key);
+            return false;
+        }
+
+        return true;
+
+    }
+
+    /**
+     * Method to delete all values which represent the given board.
+     * @param {*} board the board which value should be deleted from the strategy.
+     */
+    deleteAll(board) {
+        const copy = board.deepCopy();
+        copy.getMinimalSymmetry();
+        for (const k in this.states) {
+            for (let i = 0; i < this.states[k].length; i++) {
+                const copiedMinState = this.states[k][i].deepCopy();
+                copiedMinState.getMinimalSymmetry();
+                if (JSON.stringify(copy.board) == JSON.stringify(copiedMinState.board)) {
+                    this.deletedStatesInCurrentGame.push(copy.deepCopy());
+                    delete this.states[k][i];
+                }
+            }
+            this.states[k] = this.states[k].filter(el => el);
+
+            if (this.states[k].length == 0) {
+                this.emptyKeys.push(k);
+            }
+        }
+    }
+
+    /**
+     * Method to delete all values which direct next move can lead to the given key.
+     * @param {*} key the key that the values which need to be deleted could reach.
+     */
+    deleteValuesLeadingToKey(key) {
+        const valToDelete = [];
+        for (const k in this.states) {
+            const len = this.states[k].length;
+            for (let i = 0; i < len; i++) {
+                const follow = this.states[k][i].getAllSymmetricalNextBoards();
+                for (let j = 0; j < follow.length; j++) {
+                    const copiedMinFollow = follow[j];
+                    copiedMinFollow.getMinimalSymmetry();
+                    if (JSON.stringify(copiedMinFollow.board) == key) {
+                        valToDelete.push([k, this.states[k][i].deepCopy()]);
+                    }
+                }
+            }
+        }
+        if (valToDelete.length != 0) {
+            for (let i = 0; i < valToDelete.length; i++) {
+                this.deleteAll(valToDelete[i][1]);
+            }
+        }
+    }
+
+    /**
+     * Method to create the strategy of the agent.
+     * @param {*} board the boardsituation where the next move is made by the current player.
+     * @param {*} player the player who is the current player
+     */
+    getAllPossibleStates(board, player) {
+        const winner = board.getWinner();
+        if (winner == undefined) {
+            const copy = board.deepCopy();
+            copy.getMinimalSymmetry();
+            const possibleNextBoards = copy.getAllSymmetricalNextBoards();
+
+            if (player == this.opponent && possibleNextBoards.length > 1) {
+                if (this.states[JSON.stringify(copy.board)] == undefined) {
+                    this.states[JSON.stringify(copy.board)] = possibleNextBoards;
+                }
+            }
+
+            for (let i = 0; i < possibleNextBoards.length; i++) {
+                this.getAllPossibleStates(possibleNextBoards[i], copy.currentPlayer);
+            }
+        }
+    }
+}
+
+/**
+ * Class to create an Agent with alpha-beta strategy.
+ */
+class perfectAgent {
+    constructor(playerSymbol = 2, opponent = 1) {
+        this.playerSymbol = playerSymbol;
+        this.opponent = opponent;
+    }
+    /**
+     * Method to evaluate the next best move via alpha-beta tree-pruning.
+     * @param {*} board the current board situation.
+     * @returns the best possible move on the current board situation.
+     */
+    chooseAction(board) {
+        // Call alpha beta with -inf and +inf
+        const copy = board.deepCopy();
+        const bestMove = this.alphaBeta(copy, this.playerSymbol, -2, 2);
+        return bestMove[1];
+    }
+    /**
+     * Alpha-Beta tree-pruning algorithm to evaluate the best possible move on a given board, for a player.
+     * @param {*} board the current board situation.
+     * @param {*} player the player whose best move should be evaluated.
+     * @param {*} alpha the lower bound.
+     * @param {*} beta the upper bound.
+     * @returns the best possible move for the player.
+     */
+    alphaBeta(board, player, alpha, beta) {
+        let opp
+        if (player == this.playerSymbol) {
+            opp = this.opponent;
+        } else {
+            opp = this.playerSymbol;
+        }
+
+        // case the game in current situation has ended
+        // return [Ergebnis, -]
+        const winner = board.getWinner();
+        if (winner != undefined) {
+            if (winner == player) {
+                return [1, undefined];
+            }
+
+            if (winner == opp) {
+                return [-1, undefined];
+            }
+
+            if (winner == 0) {
+                return [0, undefined];
+            }
+        }
+
+        let bestMove
+        let bestValue = alpha;
+
+        const possibleMoves = board.getAvailablePositions();
+        for (let i = 0; i < possibleMoves.length; i++) {
+
+            const copy = board.deepCopy();
+            copy.setPlayer(possibleMoves[i]);
+            const result = this.alphaBeta(copy, opp, -beta, -bestValue);
+            const value = result[0] * (-1);
+            if (value > bestValue) {
+                bestValue = value;
+                bestMove = possibleMoves[i];
+                if (bestValue >= beta) {
+                    break;
+                }
+            }
+        }
+        return [bestValue, bestMove];
+
+
+    }
+
+}
---
+++ src/app/tictactoe/gameFunctions.js
@@ -0,0 +1,230 @@
+/**
+ * Global variable for the intelligent agent.
+ */
+let agent = new Agent(1,2)
+
+/**
+ * Global variable for the alpha-beta-agent.
+ */
+let agent2 = new perfectAgent(2,1)
+
+/**
+ * Global variable to store if a player or the other agent plays.
+ */
+let againstPerfectAgent = false
+
+/**
+ * Global variable for the boardmodel.
+ */
+let board = new Board(agent.playerSymbol, agent.opponent)
+
+/**
+ * Global variable for counting the number of games already played.
+ */
+let gameCounter = 0
+
+/**
+ * Global variable for the y value of the learning curve.
+ */
+let learningCurve = 0
+
+/**
+ * Global variable for the board visualization.
+ */
+let boardCanvas
+
+/**
+ * Global variable for the gametree visualization. 
+ */
+let treeCanvas
+
+/**
+ * Global variable for the learning curve visualization.
+ */
+let diagrammCanvas
+
+/**
+ * Global variable which stores all possible situations the current game could have had. Contains arrays, where each entry is one turn of the agent or human. 
+ * If it´s an agents turn the array consist of arrays where the first entry is the possible move and the second is an indicator if
+ * the move still is possible to move (1), was chosen to move (2) or isnt available any longer (0)
+ */
+let situationsInCurrentGame = []
+//Push the initial empty board in the situations variable
+situationsInCurrentGame.push([[board.deepCopy(),1]])
+
+/**
+ * Global variable for the color mode representation
+ */
+let colorMode = "red-green"
+
+/**
+ * Creating all the Canvases on load, and start the game. 
+ */
+window.onload = function(){
+    treeCanvas = new p5(treeSketch, 'tree')
+    boardCanvas = new p5(boardSketch, 'board')
+    diagrammCanvas = new p5(diagrammSketch, 'diagram')
+    agentTurn()
+}
+
+/**
+ * Function to reset the current game, so a new game can be played.
+ */
+function resetGame(){
+    boardCanvas.noLoop()
+    treeCanvas.noLoop()
+    againstPerfectAgent = false
+    board.reset()
+    agent.reset()
+    situationsInCurrentGame = []
+    situationsInCurrentGame.push([[board.deepCopy(),1]])
+    treeCanvas.remove()
+    treeCanvas = new p5(treeSketch, 'tree')
+    boardCanvas.loop()
+    treeCanvas.loop()
+    agentTurn()
+  
+  }
+
+/**
+ * Function to makes the agents turn. 
+ */
+async function agentTurn(){
+    treeCanvas.noLoop()
+    if(board.currentPlayer == agent.playerSymbol){
+        if(againstPerfectAgent){
+            await timeout(200)//500)
+        }
+        let move = agent.chooseAction(board)
+        addSituationsToGameTree(agent, board, move, situationsInCurrentGame)
+        board.setPlayer(move)
+        winner = board.getWinner()
+    }
+    if(winner != undefined){
+        gameHasEnded()
+    }else{
+        treeCanvas.loop()
+        if(againstPerfectAgent){
+            perfectAgentTurn()
+        }
+    }
+
+
+}
+
+/**
+ * Function to let the Agent learn and manage the learningcurve.
+ */
+function gameHasEnded(){
+    treeCanvas.noLoop()
+    let winner = board.getWinner()
+    //Game has ended
+    agent.learn(winner)
+    gameCounter++
+    document.getElementById('gameCounter').innerHTML = gameCounter
+    if(winner == agent.opponent){
+        learningCurve-=1
+    }
+    
+    if(winner == agent.playerSymbol){
+        learningCurve+=1
+    }
+    
+    if(winner == 0){
+        learningCurve+=1           
+    }  
+    treeCanvas.loop()
+    enableBtns()
+}
+
+/**
+ * Function which is called, when the human player made a move.
+ */
+function playerTurn(move){
+    treeCanvas.noLoop()
+    if(board.board[move[0]][move[1]] == 0){
+        board.setPlayer([move[0],move[1]])
+        situationsInCurrentGame.push([[board.deepCopy(),1]])            
+        winner = board.getWinner()
+    }
+    if(winner != undefined){
+        gameHasEnded()
+    }else{
+        agentTurn()
+    }
+}
+
+/**
+ * Function which makes the move for the alpha-beta agent.
+ */
+async function perfectAgentTurn(){
+    try{
+        if(board.currentPlayer == agent.opponent){
+            await timeout(200)//500)
+            let move = agent2.chooseAction(board)
+            board.setPlayer(move)
+            situationsInCurrentGame.push([[board.deepCopy(),1]]) 
+            let winner = board.getWinner()
+
+            if(winner == undefined){
+                agentTurn()
+            }else{
+                gameHasEnded()
+        
+            }           
+        }
+    }catch(err){}
+}
+
+/**
+ * Function to set everything up for a game against the alpha-beta agent.
+ */
+function perfectAgentPlay(){
+    if(!againstPerfectAgent && board.getWinner() == undefined){
+        disableBtns()
+        againstPerfectAgent = true
+        perfectAgentTurn()
+    }else{
+        resetGame()
+        perfectAgentPlay()
+    }
+
+}
+
+/**
+ * Function to reload the page.
+ */
+function resetAll(){
+    location.reload()
+}
+
+/**
+ * Function to switch the color mode from red-green to blue-yellow and vice versa.
+ */
+function switchColorMode(){
+    if(colorMode == "red-green"){
+        colorMode = "blue-yellow"
+    }else{
+        colorMode = "red-green"
+    }
+}
+
+/**
+ * Function to disable all buttons.
+ */
+async function disableBtns(){
+    const buttons = document.querySelectorAll('button')
+    for(let button of buttons){
+        button.disabled = true
+    }
+}
+
+/**
+ * Function to enable all buttons.
+ */
+function enableBtns(){
+    const buttons = document.querySelectorAll('button')
+    for(let button of buttons){
+        button.disabled = false
+    }
+}
