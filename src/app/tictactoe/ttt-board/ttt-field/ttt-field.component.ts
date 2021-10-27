a1c8c15e3b1fa5f1b8d01482f1717d1c236dd9aa
Lukas Boettcher <lukas@btchr.de>
2021-10-27 21:34:23 +0200

    added basic canvas functions to manage the display

--- src/app/tictactoe/ttt-board/ttt-field/ttt-field.component.ts
+++ src/app/tictactoe/ttt-board/ttt-field/ttt-field.component.ts
@@ -24,4 +24,30 @@ export class TttFieldComponent implements OnInit {
   }
 
   }
+
+  private drawCross(): void {
+    this.ctx.lineCap = 'round';
+    this.ctx.lineWidth = this.strokeWidth;
+    this.ctx.beginPath();
+    this.ctx.moveTo(this.strokeWidth, this.strokeWidth);
+    this.ctx.lineTo(this.width - this.strokeWidth, this.height - this.strokeWidth);
+    this.ctx.stroke();
+    this.ctx.beginPath();
+    this.ctx.moveTo(this.strokeWidth, this.height - this.strokeWidth);
+    this.ctx.lineTo(this.width - this.strokeWidth, this.strokeWidth);
+    this.ctx.stroke();
+  }
+
+  private drawCircle(): void {
+    this.ctx.lineCap = 'round';
+    this.ctx.lineWidth = this.strokeWidth;
+    this.ctx.beginPath();
+    this.ctx.arc(this.width / 2, this.height / 2, Math.min(this.width / 2, this.height / 2) - this.strokeWidth, 0, 2 * Math.PI);
+    this.ctx.stroke();
+  }
+
+  private clearCanvas(): void {
+    this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+  }
+
 }
