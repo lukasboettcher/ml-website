26bd2dc2ee5e5b36b58ad676dc6699151a8672db
Lukas Boettcher <lukas@btchr.de>
2021-10-27 21:34:01 +0200

    added basic ttt canvas references

--- src/app/tictactoe/ttt-board/ttt-field/ttt-field.component.ts
+++ src/app/tictactoe/ttt-board/ttt-field/ttt-field.component.ts
@@ -7,9 +7,21 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 })
 export class TttFieldComponent implements OnInit {
 
+  @ViewChild('canvas', { static: true })
+  canvas: ElementRef<HTMLCanvasElement>;
+  private ctx: CanvasRenderingContext2D;
+  private width = 300;
+  private height = 300;
+  private strokeWidth = 30;
+  private state = '';
 
   constructor() { }
 
   ngOnInit(): void {
+    this.canvas.nativeElement.width = this.width;
+    this.canvas.nativeElement.height = this.height;
+    this.ctx = this.canvas.nativeElement.getContext('2d');
+  }
+
   }
 }
