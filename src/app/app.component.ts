d3ce59dd83e6122659630d89bc0cb386bfa6f653
Lukas Boettcher <stu210239@mail.uni-kiel.de>
2021-12-01 10:01:47 +0100

    check the route in app component then render navbar conditionally

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ml-website';
+
+  constructor(private router: Router) { }
+
+  isSurvey(s: string): boolean {
+    return this.router.url.includes(s);
+  }
+
 }
