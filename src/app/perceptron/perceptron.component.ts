bb3418373a02590f1be3238d2549fc9707af36fe
Lukas Boettcher <stu210239@mail.uni-kiel.de>
2022-03-02 02:38:27 +0100

    sanitize the url before displaying the iframe this needs to be done explicitly, else angular prevent unsafe 3rd party frames

--- src/app/perceptron/perceptron.component.ts
+++ src/app/perceptron/perceptron.component.ts
@@ -1,4 +1,6 @@
 import { Component, HostListener, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+
 @Component({
   selector: 'app-perceptron',
   templateUrl: './perceptron.component.html',
@@ -6,5 +8,20 @@ import { Component, HostListener, OnInit } from '@angular/core';
 })
 export class PerceptronComponent implements OnInit {
 
+  url = 'https://progly.informatik.uni-kiel.de/perceptron/';
+  urlSafe: SafeResourceUrl;
+  height = 512;
+  heightRatio = 0.95;
+
+  constructor(private sanitizer: DomSanitizer) { }
+
+  ngOnInit(): void {
+    this.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl(this.url);
+    console.warn(location.href);
+    this.calculateHeight(window.innerHeight);
+  }
+  calculateHeight(windowHeight: number): void {
+    this.height = this.heightRatio * windowHeight - 100;
+  }
 
 }
