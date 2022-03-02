d4978143f7229b5896eed643361e5cd604ddf7a2
Lukas Boettcher <stu210239@mail.uni-kiel.de>
2022-03-02 02:34:43 +0100

    added perceptron wrapper component

---
+++ src/app/perceptron/perceptron.component.ts
@@ -0,0 +1,10 @@
+import { Component, HostListener, OnInit } from '@angular/core';
+@Component({
+  selector: 'app-perceptron',
+  templateUrl: './perceptron.component.html',
+  styleUrls: ['./perceptron.component.css']
+})
+export class PerceptronComponent implements OnInit {
+
+
+}
