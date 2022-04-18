f2078d5aed09aeed0bb807ca08719936776edd0c
Lukas Boettcher <stu210239@mail.uni-kiel.de>
2022-04-18 21:43:53 +0200

    added tags to every component only top level categorization into ml topics

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,26 +14,30 @@ export class HomeComponent implements OnInit {
       routerLink: '/mnist',
       text: `MNIST ist eine Datenbank von handschriftlich geschriebenen Zahlen. Diese werden verwendet, um ein neuronales Netz zu trainieren, welches in der Lage ist, handgeschriebene Zahlen zu erkennen.`,
       srcPath: 'assets/home-images/MnistExamples.png',
-      video: false
+      video: false,
+      tags: ['basic', 'supervised']
     }, {
       title: 'Objekterkennung',
       routerLink: '/object',
       text: `Diese Komponente demonstriert, wie bereits trainierte Modelle für verschiedene Anwendungsfälle verwendet werden können.
       Konkret werden Objekte durch eine Kamera erkannt.`,
       srcPath: 'assets/home-images/obj-detect-driving.webm',
-      video: true
+      video: true,
+      tags: ['basic', 'supervised', 'webcam']
     }, {
       title: 'Klassifizierung',
       routerLink: '/classify',
       text: `In der Komponente „Klassifizierung“ wird außerdem die Idee des Transfer-Lernens verwendet, um ein bestehendes Modell in seiner Funktionalität zu erweitern und anzupassen. `,
       srcPath: 'assets/home-images/classify.png',
-      video: false
+      video: false,
+      tags: ['advanced', 'supervised', 'webcam']
     }, {
       title: 'Tic Tac Toe',
       routerLink: '/tictactoe',
       text: `Hier kannst du gegen den Computer TicTacToe spielen und dabei zusehen, wie dieser dazulernt und seine Entscheidungen trifft. `,
       srcPath: 'assets/home-images/tic-tac-toe.webp',
-      video: false
+      video: false,
+      tags: ['basic', 'reinforcement']
     }, {
       title: 'Cartpole',
       routerLink: '/cartpole',
@@ -41,32 +45,37 @@ export class HomeComponent implements OnInit {
       so lange wie möglich zu balancieren. In dieser Komponente wird ein neuronales Netz
       trainiert, um dieses Spiel bestmöglich zu absolvieren.`,
       srcPath: 'assets/home-images/cartpole.gif',
-      video: false
+      video: false,
+      tags: ['advanced', 'reinforcement']
     }, {
       title: 'Das Perceptron',
       routerLink: '/perceptron',
       text: `Hier kannst du in einer interaktiven Umgebung schrittweise die Grundlagen eines Perceptrons erfahren.
       Perceptrons sind die elementaren Bausteine aus denen ein neuronales Netz besteht und sind für alle Entscheidungen verantwortlich.`,
       srcPath: 'assets/home-images/perceptron.png',
-      video: false
+      video: false,
+      tags: ['basic', 'supervised']
     }, {
       title: 'GAN Training',
       routerLink: '/gan-training',
       text: `Was sind GANs? Und wofür können diese verwendet werden?`,
       srcPath: 'assets/home-images/gan-training.webp',
-      video: false
+      video: false,
+      tags: ['basic', 'gan', 'unsupervised']
     }, {
       title: 'Style Transfer',
       routerLink: '/gan-transfer',
       text: `Verwende eine GAN basierte KI, um den Stil eines Bildes auf ein neues Bild zu übertragen.`,
       srcPath: 'assets/home-images/style.webp',
-      video: false
+      video: false,
+      tags: ['advanced', 'gan', 'unsupervised', 'webcam']
     }, {
       title: 'Landschaften erzeugen',
       routerLink: '/gan-landscpe',
       text: `Zeichne einen Sketch einer Landschaft und lass den Computer die Zeichnung interpretieren.`,
       srcPath: 'assets/home-images/landscape.gif',
-      video: false
+      video: false,
+      tags: ['advanced', 'gan', 'unsupervised']
     },
   ];
 
