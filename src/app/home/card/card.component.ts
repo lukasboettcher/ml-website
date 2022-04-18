81a26e2b0fdd45f092f438177ed4bb546c932f3e
Lukas Boettcher <stu210239@mail.uni-kiel.de>
2022-04-18 18:03:10 +0200

    added a card component for the new home design takes title, text and srcpath as input parameters

---
+++ src/app/home/card/card.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { input } from '@tensorflow/tfjs';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
