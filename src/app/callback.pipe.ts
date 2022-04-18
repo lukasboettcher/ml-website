1eee0711280ee9e46c9b78e5e5d93e3f071423b6
Lukas Boettcher <stu210239@mail.uni-kiel.de>
2022-04-18 20:23:11 +0200

    make the pipe more general, allow passing arbitrary number of arguments

--- src/app/callback.pipe.ts
+++ src/app/callback.pipe.ts
@@ -5,11 +5,14 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class CallbackPipe implements PipeTransform {
 
-  transform(items: any[], callback: (item: any) => boolean): any {
+  // this pipe takes a callback
+  // the callback decides for each item whether T/F
+  // also, all parameters are passed to the callback if possible
+  transform(items: any[], callback: (item: any, ...args) => boolean, ...args): any {
     if (!items || !callback) {
       return items;
     }
-    return items.filter(item => callback(item));
+    return items.filter(item => callback(item, ...args));
   }
 
 }
