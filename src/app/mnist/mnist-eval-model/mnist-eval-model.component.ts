97453cdf0e1d51eaeb35b93fa402d450b5ddba17
Lukas Boettcher <stu210239@mail.uni-kiel.de>
2022-06-10 21:53:16 +0200

    import tfjs components needed for evaluating model also use model from input

--- src/app/mnist/mnist-eval-model/mnist-eval-model.component.ts
+++ src/app/mnist/mnist-eval-model/mnist-eval-model.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, ViewChild } from '@angular/core';
+import * as tf from '@tensorflow/tfjs';
+import { Tensor } from '@tensorflow/tfjs';
 import { ChartConfiguration, ChartType } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
 
@@ -8,6 +10,15 @@ import { BaseChartDirective } from 'ng2-charts';
   styleUrls: ['./mnist-eval-model.component.css']
 })
 export class MnistEvalModelComponent {
+
+  @ViewChild(BaseChartDirective)
+  chart?: BaseChartDirective;
+  @Input() model: any;
+
+  results: number[];
+  prediction: number;
+
+
   barChartData: ChartConfiguration['data'] = {
     datasets: [
       {
@@ -37,4 +48,45 @@ export class MnistEvalModelComponent {
 
   constructor() { }
 
+  // get canvas image and interpret it
+  // save results afterwards
+  onClassify(i: ImageData): void {
+    this.convertCanvasTensor(i).then(
+      t => {
+        const prediction = this.model.predict(t);
+        this.results = Array.from(prediction.dataSync());
+        this.barChartData.datasets[0].data = this.results;
+        this.prediction = this.labelData(this.results);
+        this.chart?.update();
+      });
+  }
+
+  // convert image data into a canvas that can be interpreted by tensorflow
+  async convertCanvasTensor(i: ImageData): Promise<Tensor> {
+    const unit = tf.browser.fromPixels(i)
+      // convert 3d tensor to 2d tensor
+      .resizeNearestNeighbor([28, 28])
+      // normalize data
+      .mean(2).expandDims(2).expandDims()
+      // flatten for nnet input
+      .toFloat()
+      // scale [0,255] -> [0,1]
+      .div(255.);
+    return unit;
+  }
+
+  // find the number for the highest probability
+  labelData(d: Array<number>): number {
+    // find label for most likely number
+    let prob = d[0];
+    let label = 0;
+
+    for (let i = 1; i < d.length; i++) {
+      if (d[i] > prob) {
+        label = i;
+        prob = d[i];
+      }
+    }
+    return label;
+  }
 }
