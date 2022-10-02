dd4e9e0efe4b2ddd4005fb95d0b87451b3b760a5
Lukas Boettcher <stu210239@mail.uni-kiel.de>
2022-10-02 10:50:09 +0200

    add a minimal example to qna component to try out the functionality

--- src/app/text/qna/qna.component.ts
+++ src/app/text/qna/qna.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as qna from '@tensorflow-models/qna';
+
 @Component({
   selector: 'app-qna',
   templateUrl: './qna.component.html',
@@ -9,6 +11,13 @@ export class QnaComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const passage = 'Google LLC is an American multinational technology company that specializes in Internet-related services and products, which include online advertising technologies, search engine, cloud computing, software, and hardware. It is considered one of the Big Four technology companies, alongside Amazon, Apple, and Facebook. Google was founded in September 1998 by Larry Page and Sergey Brin while they were Ph.D. students at Stanford University in California. Together they own about 14 percent of its shares and control 56 percent of the stockholder voting power through supervoting stock. They incorporated Google as a California privately held company on September 4, 1998, in California. Google was then reincorporated in Delaware on October 22, 2002. An initial public offering (IPO) took place on August 19, 2004, and Google moved to its headquarters in Mountain View, California, nicknamed the Googleplex. In August 2015, Google announced plans to reorganize its various interests as a conglomerate called Alphabet Inc. Google is Alphabet\'s leading subsidiary and will continue to be the umbrella company for Alphabet\'s Internet interests. Sundar Pichai was appointed CEO of Google, replacing Larry Page who became the CEO of Alphabet.';
+    const question = 'Who is the CEO of Google?';
+    qna.load({modelUrl: 'assets/text/qna/model.json'}).then(model => {
+      model.findAnswers(question, passage).then(answer => {
+        console.log(answer);
+      });
+    });
   }
 
 }
