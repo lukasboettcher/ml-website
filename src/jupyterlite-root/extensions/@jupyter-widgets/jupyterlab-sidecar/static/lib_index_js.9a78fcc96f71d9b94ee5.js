08645abe9b4590173cacfd558edb77a5b5267fa8
Lukas Boettcher <stu210239@mail.uni-kiel.de>
2023-01-03 02:06:29 +0100

    add initial jupyterlite root to source control

---
+++ src/jupyterlite-root/extensions/@jupyter-widgets/jupyterlab-sidecar/static/lib_index_js.9a78fcc96f71d9b94ee5.js
@@ -0,0 +1,30 @@
+(self["webpackChunk_jupyter_widgets_jupyterlab_sidecar"] = self["webpackChunk_jupyter_widgets_jupyterlab_sidecar"] || []).push([["lib_index_js"],{
+
+/***/ "./lib/index.js":
+/*!**********************!*\
+  !*** ./lib/index.js ***!
+  \**********************/
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+"use strict";
+
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+__exportStar(__webpack_require__(/*! ./plugin */ "./lib/plugin.js"), exports);
+__exportStar(__webpack_require__(/*! ./version */ "./lib/version.js"), exports);
+__exportStar(__webpack_require__(/*! ./widget */ "./lib/widget.js"), exports);
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=lib_index_js.9a78fcc96f71d9b94ee5.js.map
