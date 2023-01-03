a600cdc31421df165f12a4dc508d4fa7a82fb206
Lukas Boettcher <stu210239@mail.uni-kiel.de>
2023-01-03 01:55:53 +0100

    create a build script for jupyterlite

---
+++ src/jupyterlite/build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+SCRIPT_DIR=$(dirname $0)
+echo $SCRIPT_DIR
+if [ ! -d "$SCRIPT_DIR/venv" ]; then
+    echo "$SCRIPT_DIR/venv does not exist"
+    python3 -m venv $SCRIPT_DIR/venv
+fi
+. $SCRIPT_DIR/venv/bin/activate
+echo "installing packages into venv $(which pip)"
+pip install -r $SCRIPT_DIR/requirements.txt
+echo "building jupyterlite output"
+jupyter lite build --no-sourcemaps --no-unused-shared-packages --lite-dir $SCRIPT_DIR --output-dir $SCRIPT_DIR/../jupyterlite-root --base-url 'ki-labor/jupyterlite-root/'
+echo "done"
+deactivate
